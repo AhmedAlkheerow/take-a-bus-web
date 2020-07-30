3a22d03d9bafd47120c14821b4ffb8fa545332d4
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-07-30 12:46:46 +0300

    Feat: Create sign in/up modal Fixes #11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1 className="text-2xl bg-lightblue">Take a Bus</h1>
-      <h3>Added the map</h3>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
