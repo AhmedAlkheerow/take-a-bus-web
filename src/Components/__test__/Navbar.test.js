3642447e8766d6ff4b47034bb8b522a5e3ea9c13
Wisam Naji <guitar90rocker@gmail.com>
2020-08-05 17:35:46 +0300

    Deleted test inside Components/

--- src/Components/__test__/Navbar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Navbar from '../Navbar';
-
-test('renders the navbar', () => {
-  const { container } = render(<Navbar />);
-  const navbarContainer = container.querySelector('div');
-  expect(navbarContainer).toBeInTheDocument();
-});
