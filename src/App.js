b5b4a7d527528b34083169a742f56f17c149ceab
Wisam Naji <guitar90rocker@gmail.com>
2020-08-06 14:28:48 +0300

    Feat: Create sign in/up modal #11 reverted App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import Map from './components/MapContainer';
 import Navbar from './components/Navbar';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-
 function App() {
   return (
     <div>
       <Navbar />
       <Map />
-      <SignIn />
-      <SignUp />
     </div>
   );
 }
