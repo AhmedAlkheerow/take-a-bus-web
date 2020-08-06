037bc3b7f269b21418647e3a86250da41de58021
Wisam Naji <guitar90rocker@gmail.com>
2020-08-06 21:05:17 +0300

    tweaked styling

--- src/components/BreakLine.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import '../tailwind.css';
-
-export default function BreakLine() {
-  return <hr className="border-b-2 border-gray-200"></hr>;
-}
--- src/components/SuggestionsContainer.js
+++ src/components/SuggestionsContainer.js
@@ -1,38 +1,39 @@
 import React from 'react';
-import '../../tailwind.css';
 import SuggestionItem from './SuggestionItem';
-import BreakLine from '../BreakLine';
+
+const suggestions = [
+  {
+    busNo: '473824 EBL - IRQ',
+    origin: 'Sami Abdulrahman',
+    destination: 'Erbil Citadel',
+    duration: '6:45 PM - 8:15 PM',
+  },
+  {
+    busNo: '473824 EBL - IRQ',
+    origin: 'Sami Abdulrahman',
+    destination: 'Erbil Citadel',
+    duration: '6:45 PM - 8:15 PM',
+  },
+  {
+    busNo: '473824 EBL - IRQ',
+    origin: 'Sami Abdulrahman',
+    destination: 'Erbil Citadel',
+    duration: '6:45 PM - 8:15 PM',
+  },
+  {
+    busNo: '473824 EBL - IRQ',
+    origin: 'Sami Abdulrahman',
+    destination: 'Erbil Citadel',
+    duration: '6:45 PM - 8:15 PM',
+  },
+];
 
 export default function SuggestionContainer() {
   return (
-    <div className="w-full sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/3 ">
-      <SuggestionItem
-        busNo="473824 EBL - IRQ"
-        origin="Sami Abdulrahman"
-        destination="Erbil Citadel"
-        duration="6:45 PM - 8:15 PM"
-      />
-      <BreakLine />
-      <SuggestionItem
-        busNo="473824 EBL - IRQ"
-        origin="Sami Abdulrahman"
-        destination="Erbil Citadel"
-        duration="6:45 PM - 8:15 PM"
-      />
-      <BreakLine />
-      <SuggestionItem
-        busNo="473824 EBL - IRQ"
-        origin="Sami Abdulrahman"
-        destination="Erbil Citadel"
-        duration="6:45 PM - 8:15 PM"
-      />
-      <BreakLine />
-      <SuggestionItem
-        busNo="473824 EBL - IRQ"
-        origin="Sami Abdulrahman"
-        destination="Erbil Citadel"
-        duration="6:45 PM - 8:15 PM"
-      />
+    <div className="w-1/3">
+      {suggestions.map((s, idx) => (
+        <SuggestionItem {...s} key={idx} />
+      ))}
     </div>
   );
 }
