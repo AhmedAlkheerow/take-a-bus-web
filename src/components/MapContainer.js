bec4d62d1c9908fac038ebea7a5ec58b85b96aab
Wisam Naji <guitar90rocker@gmail.com>
2020-08-07 21:36:07 +0300

    fixed map sizing on viewport change Feat: Create the layout component that will be the root of our pages #18

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactMapGL from 'react-map-gl';
 
 export default function Map() {
@@ -6,16 +6,19 @@ export default function Map() {
     latitude: 36.206291,
     longitude: 44.008869,
     width: '100%',
-    height: '100vh',
+    height: '100%',
     zoom: 11,
   });
+
+  const map = useRef();
   return (
     <>
       <ReactMapGL
         {...viewport}
+        ref={map}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         onViewportChange={(viewport) => {
-          setViewport(viewport);
+          setViewport({ ...viewport, width: '100%', height: '100%' });
         }}
         mapStyle="mapbox://styles/shna/ckd4x2xmy02kh1ir3hihcr36m"
       ></ReactMapGL>
