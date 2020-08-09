f2c67d753dca918a01ff530c97a8001b6c1dc949
JalalArif <jalal.00066564@gmail.com>
2020-08-09 14:22:39 +0300

    Feat: Create a BusList component Fixes #24

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Map from './Components/MapContainer';
 import Navbar from './Components/Navbar';
-import BusList from './Components/AvailableBuses/AvailableBusesList';
+import BusList from './Components/AvailableBusesList';
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      {/* <Navbar />
+      <Map /> */}
+      <BusList />
+    </div>
+  );
 }
 
 export default App;
--- src/components/AvailableBuses/ShiftTime.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function ShiftTime() {
-  return <div></div>;
-}
