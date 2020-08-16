df276ffb27c4da1c14571eb8bcf9796a7332acc7
Wisam Naji <guitar90rocker@gmail.com>
2020-08-16 15:11:42 +0300

    reverted MapContainer.js

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -17,7 +17,7 @@ export default function Map() {
         onViewportChange={(viewport) => {
           setViewport(viewport);
         }}
-        mapStyle="mapbox://styles/jalalsoft/ckdjf85k204gq1imvqrmgwtgu"
+        mapStyle="mapbox://styles/shna/ckd4x2xmy02kh1ir3hihcr36m"
       >
         <div className="h-full flex flex-row-reverse items-end p-16">
           <LocateMeBtn />
