261267b51bfd6a2c45f10f2146212ac397f3d6dd
shna-rafeeq <shnar.majeed@gmail.com>
2020-08-22 23:08:54 +0300

    Feat: Create the cloud function that exposes an endpoint which suggest the best route Fixes #70 importing cors package

--- api/functions/index.js
+++ api/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
 const firebaseConfig = {
   apiKey: 'AIzaSyC5oUqe1Y0-ZGy_tkTG7jwWx98-LGiVMRs',
   authDomain: 'mapsproject-228715.firebaseapp.com',
@@ -19,31 +20,33 @@ const lodash = require('lodash');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest(async (request, response) => {
-  if (request.method === 'POST') {
-    let routes = [];
-    const lat = request.body.lat;
-    const long = request.body.long;
-    const point = turf.point([lat, long]);
-    const snapshot = await admin.firestore().collection('routes').get();
-    const docs = snapshot.docs;
-    docs.forEach((doc) => {
-      const docData = doc.data();
-      const pathObject = JSON.parse(docData.path);
-      const lineArray = pathObject.geometry.coordinates;
-      const line = turf.lineString(lineArray);
-      routes.push({
-        ...docData,
-        path: pathObject,
-        distance: turf.pointToLineDistance(point, line, { units: 'meters' }),
+  cors(request, response, async () => {
+    if (request.method === 'POST') {
+      let routes = [];
+      const lat = request.body.lat;
+      const long = request.body.long;
+      const point = turf.point([lat, long]);
+      const snapshot = await admin.firestore().collection('routes').get();
+      const docs = snapshot.docs;
+      docs.forEach((doc) => {
+        const docData = doc.data();
+        const pathObject = JSON.parse(docData.path);
+        const lineArray = pathObject.geometry.coordinates;
+        const line = turf.lineString(lineArray);
+        routes.push({
+          ...docData,
+          path: pathObject,
+          distance: turf.pointToLineDistance(point, line, { units: 'meters' }),
+        });
       });
-    });
-    const sortedRoutes = lodash.sortBy(routes, ['distance'], ['asc']);
-    response.send({
-      route: sortedRoutes[0],
-      lat,
-      long,
-    });
-  } else {
-    response.send('Unknown Method');
-  }
+      const sortedRoutes = lodash.sortBy(routes, ['distance'], ['asc']);
+      response.send({
+        route: sortedRoutes[0],
+        lat,
+        long,
+      });
+    } else {
+      response.send('Unknown Method');
+    }
+  });
 });
