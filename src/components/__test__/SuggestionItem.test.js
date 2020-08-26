994f681ee9b69f3f9d76eaa3c01b099df20a50ed
Wisam Naji <guitar90rocker@gmail.com>
2020-08-26 14:38:44 +0300

    fixed the test

--- src/components/__test__/SuggestionItem.test.js
+++ src/components/__test__/SuggestionItem.test.js
@@ -3,5 +3,14 @@ import { render } from '@testing-library/react';
 import SuggestionItem from '../SuggestionItem';
 
 test('renders the SuggestionItem', () => {
-  render(<SuggestionItem />);
+  const props = {
+    working_hours: ['0050-2030'],
+    originToBusDistance: 0.5,
+    originToBusTime: 2,
+    tripDistance: 5,
+    tripEST: 15,
+    plate_number: '545454',
+    plate_region: 'Erbil',
+  };
+  render(<SuggestionItem {...props} />);
 });
